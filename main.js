20e76cc162392e76c17cb544fb0a6c7a0c852786
Arykelmys <108533403+Arykelmys@users.noreply.github.com>
2024-04-11 14:30:55 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-04-05T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempoObjetivo1 = new Date("2024-04-15T00:00:00");
+const tempoObjetivo2 = new Date("2024-04-15T00:00:00");
+const tempoObjetivo3 = new Date("2024-04-15T00:00:00");
+const tempoObjetivo4 = new Date("2024-04-15T00:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
@@ -17,7 +22,12 @@ function calculaTempo(tempoObjetivo){
     minutos %= 60;
     horas %= 24;
 
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    if (tempoFinal>0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    }else{
+        return "Prazo Finalizado"
+    }
+    
 }
 
 for (let i=0; i < botoes.length; i++){
@@ -32,3 +42,18 @@ botoes[i].classList.add("ativo");
 textos[i].classList.add("ativo");
     }
 }
+
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length; i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
